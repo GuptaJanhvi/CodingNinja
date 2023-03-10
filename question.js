bc9f7852fe46b53cbd59231f49e8f701e8544ebc
GuptaJanhvi <113235149+GuptaJanhvi@users.noreply.github.com>
2023-03-10 11:10:19 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,47 @@
+let questions = [
+  {
+  numb: 1,
+  question: "HTML documents are saved in",
+  answer: "ASCII text",
+  options: [
+    "Special binary format",
+    "Machine language codes",
+    "ASCII text",
+    "None of above"
+  ]
+},
+  {
+  numb: 2,
+  question: "A webpage displays a picture. What tag was used to display that picture?",
+  answer: "img",
+  options: [
+    "picture",
+    "img",
+    "src",
+    "image"
+  ]
+},
+  {
+  numb: 3,
+  question: "When should you use path along with file name of picture in IMG tag?",
+  answer: "When the location of image file and html file are different",
+  options: [
+    "Path is optional and not necessary",
+    "Path is always necessary when inserting image",
+    "When image file and html file both are on same location",
+    "When the location of image file and html file are different"
+  ]
+},
+  {
+  numb: 4,
+  question: "The special formatting codes in HTML document used to present cotags",
+  answer: "tags",
+  options: [
+    "values",
+    "tags",
+    "attributes",
+    "None of above "
+  ]
+},
+  
+];
